33841ac862f3652010a7306bee7074f776a6d1c8
Iffat Rahman <iffat.rahman92@gmail.com>
2020-10-12 11:52:18 -0400

    Create styled Signin UI

--- signin/views.py
+++ signin/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import *
 from django.contrib.auth import *
 from django.contrib import messages
 
-def signin(request):
 
+def signin(request):
     if request.method == 'POST':
         form = AuthenticationForm(request=request, data=request.POST)
         if form.is_valid():
@@ -20,6 +20,6 @@ def signin(request):
         else:
             messages.error(request, "Invalid username or password.")
     form = AuthenticationForm()
-    return render(request = request,
-                    template_name = "signin/signin.html",
-                    context={"form":form})
+    return render(request=request,
+                  template_name="signin/signin.html",
+                  context={"form": form})
