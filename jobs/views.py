cd663912d7b5b69ed81613b9e647c61f6822a9fe
Shivang <shivang@Shivangs-MacBook-Pro.local>
2020-10-16 17:13:25 -0400

    Added module for Getting Data from API and Show the values in a table

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .getdata import getJobs
 
-# Create your views here.
+from django.shortcuts import render, redirect, reverse
+from django.template.loader import render_to_string
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.views import View
+from register.forms import SignUpForm
+from django.core.mail import EmailMessage
+from django.contrib import messages
+from django.contrib.sites.shortcuts import get_current_site
+
+class GetData(TemplateView):
+    template_name = 'jobs.html'
+    def get_context_data(self, *args, **kwargs):
+        context = {
+            'jobs' : getJobs(),
+        }
+        return context
