427aaa2bfce57e87fbfb8a50ec604cefe06a65d1
Gaurav Agrawal <gauravag2207@gmail.com>
2020-10-20 01:24:54 -0500

    Created Job Urls, updated job view, added heading to HTML

--- jobs/views.py
+++ jobs/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from .getdata import getJobs
-
-from django.shortcuts import render, redirect, reverse
-from django.template.loader import render_to_string
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from django.views import View
-from register.forms import SignUpForm
-from django.core.mail import EmailMessage
-from django.contrib import messages
-from django.contrib.sites.shortcuts import get_current_site
 from .models import job_record
 
-data = job_record.objects.all()
 
-
-class GetData(TemplateView):
-    template_name = "jobs.html"
-
+class JobsView(TemplateView):
+    template_name = "jobs/jobs.html"
     def get_context_data(self, *args, **kwargs):
         context = {
-            "jobs": data,
+            "jobs": job_record.objects.all(),
         }
         return context
