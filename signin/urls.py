5f0fdb1899e122ead19de28f5c05e8fd6be492c6
Gaurav Agrawal <gauravag2207@gmail.com>
2020-10-24 03:47:15 -0500

    Formated with Black

--- signin/urls.py
+++ signin/urls.py
@@ -19,44 +19,37 @@ from signin.views import *
 
 app_name = "signin"
 urlpatterns = [
-    path(
-        "", SignInView.as_view(), name="signin"
-    ),
-
-    path(
-        "success", SuccessView.as_view(), name="success"
-    ),
-
+    path("", SignInView.as_view(), name="signin"),
+    path("success", SuccessView.as_view(), name="success"),
     path(
         "password_reset/",
         auth_views.PasswordResetView.as_view(
             template_name="signin/password_reset.html",
             email_template_name="signin/password_reset_email.html",
-            success_url=reverse_lazy("signin:password_reset_done")
+            success_url=reverse_lazy("signin:password_reset_done"),
         ),
-        name="password_reset"
+        name="password_reset",
     ),
-
     path(
         "password_reset_done/",
         auth_views.PasswordResetDoneView.as_view(
-            template_name="signin/password_reset_sent.html"),
-        name="password_reset_done"
+            template_name="signin/password_reset_sent.html"
+        ),
+        name="password_reset_done",
     ),
-
     path(
         "password_reset_confirm/<uidb64>/<token>/",
         auth_views.PasswordResetConfirmView.as_view(
             template_name="signin/password_reset_confirm.html",
-            success_url=reverse_lazy("signin:password_reset_complete")
+            success_url=reverse_lazy("signin:password_reset_complete"),
         ),
-        name="password_reset_confirm"
+        name="password_reset_confirm",
     ),
-
     path(
-        'password_reset_complete/',
+        "password_reset_complete/",
         auth_views.PasswordResetCompleteView.as_view(
-            template_name="signin/password_reset_complete.html"),
-        name="password_reset_complete"
+            template_name="signin/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
     ),
 ]
--- signin/views.py
+++ signin/views.py
@@ -9,38 +9,40 @@ from django.contrib.auth.decorators import login_required
 
 class SignInView(FormView):
     form_class = AuthenticationForm
-    template_name = 'signin/signin.html'
+    template_name = "signin/signin.html"
 
     def form_valid(self, form):
         # """
         # The user has provided valid credentials (this was checked in AuthenticationForm.is_valid()). So now we
         # can log him in.
         # """
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password')
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
         user = authenticate(username=username, password=password)
 
         if user is not None:
             login(self.request, user)
             # messages.info(self.request, f"You are now logged in as {username}")
-            return redirect(reverse('signin:success'))
+            return redirect(reverse("signin:success"))
         else:
             messages.error(self.request, "Invalid username or password.")
 
     def form_invalid(self, form):
         messages.error(self.request, "Invalid username or password.")
         return self.render_to_response(
-            self.get_context_data(request=self.request, form=form))
+            self.get_context_data(request=self.request, form=form)
+        )
 
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect(reverse('signin:success'))
+            return redirect(reverse("signin:success"))
         return super(SignInView, self).get(request, *args, **kwargs)
 
 
 class SuccessView(View):
-
     def get(self, request, *args, **kwargs):
-        return render(request=request,
-                      template_name="signin/success.html",
-                      context={"user": request.user})
+        return render(
+            request=request,
+            template_name="signin/success.html",
+            context={"user": request.user},
+        )
