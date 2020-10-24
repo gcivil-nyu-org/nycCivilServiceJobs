a35c6cd19ca4a26fe1acd52f2d57dfb517208cb2
Iffat Rahman <iffat.rahman92@gmail.com>
2020-10-24 13:44:13 -0400

    Update register tests for better coverage

--- register/tests.py
+++ register/tests.py
@@ -1,79 +1,114 @@
 
 
 # Create your tests here.
-from django.contrib.auth.models import User
+from register.models import User
 from django.test import TestCase
-
-from register import forms
-
-
-class RegistrationFormTests(TestCase):
-    """
-    Test the default registration forms.
-    """
-    def test_registration_form(self):
-        """
-        Test that ``RegistrationForm`` enforces username constraints
-        and matching passwords.
-        """
-        # Create a user so we can verify that duplicate usernames aren't
-        # permitted.
-        User.objects.create_user('Job seeker','alice','balden' 'alice@example.com', 'alicebalden','password','password','02/09/1992','What city were you born in?','NYC')
-
-        invalid_data_dicts = [
-            # Non-alphanumeric username.
-            {'data': {'username': 'foo/bar',
-                      'email': 'foo@example.com',
-                      'password': 'foo',
-                      'password2': 'foo'},
-            'error': ('username', [u"This value must contain only letters, numbers and underscores."])},
-            # Already-existing username.
-            {'data': {'username': 'alice',
-                      'email': 'alice@example.com',
-                      'password': 'secret',
-                      'password2': 'secret'},
-            'error': ('username', [u"A user with that username already exists."])},
-            # Mismatched passwords.
-            {'data': {'username': 'foo',
-                      'email': 'foo@example.com',
-                      'password1': 'foo',
-                      'password2': 'bar'},
-            'error': ('__all__', [u"The two password fields didn't match."])},
-            ]
-
-        for invalid_dict in invalid_data_dicts:
-            form = forms.RegistrationForm(data=invalid_dict['data'])
-            self.failIf(form.is_valid())
-            self.assertEqual(form.errors[invalid_dict['error'][0]],
-                             invalid_dict['error'][1])
-
-        form = forms.RegistrationForm(data={'username': 'foo',
-                                            'email': 'foo@example.com',
-                                            'password': 'foo',
-                                            'password2': 'foo'})
-        self.failUnless(form.is_valid())
+from django.urls import reverse, reverse_lazy
+from register.forms import *
+from http import HTTPStatus
 
 
-#Test for each User having unique email
-    def test_registration_form_unique_email(self):
-        """
-        Test that ``RegistrationFormUniqueEmail`` validates uniqueness
-        of email addresses.
-        """
-        # Create a user so we can verify that duplicate addresses
-        # aren't permitted.
-        User.objects.create_user('Job seeker','alice','balden' 'alice@example.com', 'alicebalden','password','password','02/09/1992','What city were you born in?','NYC')
+class BaseTest(TestCase):
+    def setUp(self):
+        self.register_url = reverse('register:signup')
+        self.dummy_user = User.objects.create(is_hiring_manager = True, username = 'testjane',
+                                               first_name ='Jane',last_name ='Doe',
+                                               dob = '1994-10-02', email = 'janetest@username.gov',
+                                               password = 'thisisapassword')
+                
+        self.user ={
+            'is_hiring_manager': 'True',
+            'username':'testuser',
+            'first_name':'john',
+            'last_name':'doe',
+            'dob': '01/10/1992',
+            'email': 'test@username.gov',
+            'password1':'thisisapassword',
+            'password2': 'thisisapassword',
+        }
+        self.user_no_username ={
+            'is_hiring_manager': 'False',
+            'username':'',
+            'first_name':'john',
+            'last_name':'doe',
+            'dob': '01/10/1992',
+            'email': 'test@username.gov',
+            'password1':'thisisapassword',
+            'password2': 'thisisapassword',
+        }
 
-        form = forms.RegistrationFormUniqueEmail(data={'username': 'foo',
-                                                       'email': 'alice@example.com',
-                                                       'password1': 'foo',
-                                                       'password2': 'foo'})
-        self.failIf(form.is_valid())
-        self.assertEqual(form.errors['email'],
-                         [u"This email address is already in use. Please supply a different email address."])
+        self.user_name_exists ={
+            'is_hiring_manager': 'False',
+            'username':'testjane',
+            'first_name':'john',
+            'last_name':'doe',
+            'dob': '01/10/1992',
+            'email': 'test@username.com',
+            'password1':'thisisapassword',
+            'password2': 'thisisapassword',
+        }
 
-        form = forms.RegistrationFormUniqueEmail(data={'username': 'foo',
-                                                       'email': 'foo@example.com',
-                                                       'password1': 'foo',
-                                                       'password2': 'foo'})
-        self.failUnless(form.is_valid())
+        self.user_email_exists ={
+            'is_hiring_manager': 'True',
+            'username':'testjane',
+            'first_name':'john',
+            'last_name':'doe',
+            'dob': '01/10/1992',
+            'email': 'janetest@username.gov',
+            'password1':'thisisapassword',
+            'password2': 'thisisapassword',
+        }
+
+
+        self.user_hm_invalid_email ={
+            'is_hiring_manager': 'True',
+            'username':'testjane',
+            'first_name':'john',
+            'last_name':'doe',
+            'dob': '01/10/1992',
+            'email': 'jane@username.com',
+            'password1':'thisisapassword',
+            'password2': 'thisisapassword',
+        }
+
+        return super().setUp
+
+class RegisterFormTest(BaseTest):
+    
+    def test_form_not_valid(self):
+        form = SignUpForm(self.user_no_username)
+        self.assertFalse(form.is_valid())
+        
+    def test_form_valid(self):
+        form = SignUpForm(self.user)
+        self.assertTrue(form.is_valid())
+
+    def test_form_username_exists(self):
+        form = SignUpForm(self.user_name_exists)
+        form.has_error('username', 'A user with that username already exists.')
+
+    def test_form_email_exists(self):
+        form = SignUpForm(self.user_email_exists)
+        form.has_error('email', 'This Email is already registered. Please use a different email address.')
+
+    def test_form_hm_invalid_email(self):
+        form = SignUpForm(self.user_hm_invalid_email)
+        form.has_error('This email is not a valid Email Address for Hiring Manager. Please use a different email address.')
+
+
+class RegisterTest(BaseTest):
+    
+    # Setup for the test    
+    def test_can_view_register_page(self):
+        response = self.client.get(self.register_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,'register/signup.html')
+
+
+    def test_user_can_register_as_HM(self):
+        user_count = User.objects.count()
+        response = self.client.post(self.register_url,self.user, format='text/html')
+        user_count = user_count+1
+        self.assertEqual(User.objects.count(), user_count)
+        self.assertEqual(response.status_code,302)
+        self.assertTemplateUsed(response,'register/account_activated.html')
