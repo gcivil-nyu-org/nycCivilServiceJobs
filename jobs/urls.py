e2fa4845717f100585954d144bdc93f4b66d2e96
Gaurav Agrawal <gauravag2207@gmail.com>
2020-10-24 14:57:18 -0500

    Applied black and flake8 to jobs app

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from jobs.views import JobsView
-from django.views.generic import TemplateView
 
-app_name = 'jobs'
+app_name = "jobs"
 urlpatterns = [
-    path('', JobsView.as_view(), name='jobs'),
+    path("", JobsView.as_view(), name="jobs"),
 ]
