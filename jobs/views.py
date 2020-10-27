845515a91d928131a2ec19f1cc6135107e639853
Shivang Pandya <shivang.pandya01@gmail.com>
2020-10-27 17:27:49 -0400

    Updated code to handle if no quey passed

--- jobs/views.py
+++ jobs/views.py
@@ -18,10 +18,22 @@ class SearchResultsView(ListView):
     template_name = "jobs/search.html"
 
     def get_queryset(self):
-        query = self.request.GET.get("q")
-        object_list = job_record.objects.filter(
-            Q(agency__icontains=query)
-            | Q(business_title__icontains=query)
-            | Q(civil_service_title__icontains=query)
-        )
-        return object_list
+        query = self.request.GET.get("q", None)
+        if not query:
+            template_name = "jobs/jobs.html"
+
+            def get_context_data(self, *args, **kwargs):
+                context = {
+                    "jobs": job_record.objects.all()
+                    .order_by("posting_date")
+                    .reverse()[:10],
+                }
+                return context
+
+        else:
+            object_list = job_record.objects.filter(
+                Q(agency__icontains=query)
+                | Q(business_title__icontains=query)
+                | Q(civil_service_title__icontains=query)
+            )
+            return object_list
