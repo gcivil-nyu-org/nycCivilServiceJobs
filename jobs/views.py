cf26126e28ef2a0d78f5b664cc3ebd9e846ca541
Gaurav Agrawal <gauravag2207@gmail.com>
2020-10-31 22:51:43 -0500

    Added Saved jobs view and html

--- jobs/views.py
+++ jobs/views.py
@@ -1,9 +1,7 @@
-from django.views.generic import TemplateView, ListView
-from .models import job_record
+from django.views.generic import TemplateView, ListView, View
+from .models import job_record, UserSavedJob
 from django.db.models import Q
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 
 
 class JobsView(TemplateView):
@@ -13,32 +11,16 @@ class JobsView(TemplateView):
         context = {
             "jobs": job_record.objects.all().order_by("posting_date").reverse()[:10],
         }
-        return context
-
-    @login_required
-    def add_favorite(request, id):
-        job = get_object_or_404(job_record, id=id)
-        if job.favorites.filter(id=request.user.id).exists():
-            job.favorites.remove(request.user)
+        if self.request.user.is_authenticated:
+            context["saved_jobs_user"] = list(
+                UserSavedJob.objects.filter(user=self.request.user).values_list(
+                    "job", flat=True
+                )
+            )
         else:
-            job.favorites.add(request.user)
-        return HttpResponseRedirect(request.META['HTTP_REFERER'])
+            context["saved_jobs_user"] = None
 
-    @login_required
-    def favorite_list(request):
-        new = job_record.newmanager.filter(favorites=request.user)
-        return render(request,
-                      'jobs/favorites.html',
-                      {'new': new})
-
-    def job_single(request, job):
-        job = get_object_or_404(job_record)
-        is_fav = bool
-
-        if job.favorites.filter(id=request.user.id).exists():
-            is_fav = True
-
-        return render(request, 'jobs/jobs.html', {'job': job, 'is_fav': is_fav})
+        return context
 
 
 class SearchResultsView(ListView):
@@ -53,3 +35,34 @@ class SearchResultsView(ListView):
             | Q(civil_service_title__icontains=query)
         )
         return object_list
+
+
+class SaveJobsView(View):
+    def post(self, request, *args, **kwargs):
+
+        # print(request.POST['jobs_pk_id'])
+        if self.request.method == "POST":
+            job_record_pk = self.kwargs["pk"]
+            job = job_record.objects.get(pk=job_record_pk)
+            user = request.user
+            response_data = {"job_id": job_record_pk}
+            if user.is_authenticated:
+                already_saved = UserSavedJob.objects.filter(user=user, job=job)
+                if already_saved.count() == 0:
+                    save_job = UserSavedJob(user=user, job=job)
+                    save_job.save()
+                    response_data["response_data"] = "Job Saved"
+                else:
+                    already_saved.delete()
+                    response_data["response_data"] = "Job Unsaved"
+
+                # print("inside post")
+                return JsonResponse(response_data, status=200)
+
+            else:
+                # messages.error(self.request, "Invalid username or password.")
+                # print ('inside post else')
+                response_data["response_data"] = "User not authenticated"
+                return JsonResponse(response_data, status=200)
+
+            return JsonResponse({"error": ""}, status=400)
