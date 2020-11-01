b635f07f07e6fa3a766e28f478c12bf137ca42f7
Savitha <savithag1197@gmail.com>
2020-11-01 01:43:49 -0500

    Added Search Filter

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from jobs.views import JobsView
-from jobs.views import SearchResultsView
+from jobs.views import SearchResultsView, SearchFilterView
 
 app_name = "jobs"
 urlpatterns = [
     path("", JobsView.as_view(), name="jobs"),
     path("search/", SearchResultsView.as_view(), name="results"),
+    path("filter/", SearchFilterView.as_view(), name="filter"),
 ]
