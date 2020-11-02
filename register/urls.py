3de3ba5b16d6ac9e2debf034f8ba8648128ccbd8
Iffat Rahman <iffat.rahman92@gmail.com>
2020-11-02 18:53:06 -0500

    Remove unused URL path "home"

--- register/urls.py
+++ register/urls.py
@@ -15,14 +15,10 @@
 
 from django.urls import path
 from register.views import SignUpView, SuccessView
-from django.views.generic import TemplateView
 
 
 app_name = "register"
 urlpatterns = [
     path("", SignUpView.as_view(), name="signup"),
     path("success", SuccessView.as_view(), name="success"),
-    path(
-        "home", TemplateView.as_view(template_name="register/success.html"), name="home"
-    ),
 ]
