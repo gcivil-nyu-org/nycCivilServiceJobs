73e5195a9976de7ca34b2910c86d77771ece1b2a
Gaurav Agrawal <gauravag2207@gmail.com>
2020-11-02 10:36:01 -0600

    Applied black

--- signin/views.py
+++ signin/views.py
@@ -42,13 +42,17 @@ class SignInView(FormView):
 
 class SuccessView(View):
     def get(self, request, *args, **kwargs):
-        
+
         user_saved_jobs = UserSavedJob.objects.filter(user=self.request.user)
-        saved_jobs_user = list(user_saved_jobs.values_list("job", flat=True ))
+        saved_jobs_user = list(user_saved_jobs.values_list("job", flat=True))
         jobs = map(lambda x: x.job, user_saved_jobs)
 
         return render(
             request=request,
             template_name="signin/success.html",
-            context={"user": request.user, "jobs":jobs, "saved_jobs_user":saved_jobs_user},
+            context={
+                "user": request.user,
+                "jobs": jobs,
+                "saved_jobs_user": saved_jobs_user,
+            },
         )
