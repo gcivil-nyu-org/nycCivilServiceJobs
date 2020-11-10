7fc9bae300c6d8b4b92d01d886269749f26c17a8
Iffat Rahman <30580940+i-rahman@users.noreply.github.com>
2020-11-10 13:05:49 -0500

    Remove SuccessView
    SuccessView has been replaced with Dashboard

--- signin/views.py
+++ signin/views.py
@@ -5,7 +5,6 @@ from django.contrib import messages
 from django.utils.http import is_safe_url
 from django.views.generic import FormView
 from django.views import View
-from jobs.models import UserSavedJob
 from signin.forms import UserProfileForm
 from examresults.models import CivilServicesTitle
 from signin.models import UsersCivilServiceTitle
@@ -54,24 +53,6 @@ class SignInView(FormView):
         if request.user.is_authenticated:
             return redirect(reverse("dashboard:dashboard"))
         return super(SignInView, self).get(request, *args, **kwargs)
-
-
-class SuccessView(View):
-    def get(self, request, *args, **kwargs):
-
-        user_saved_jobs = UserSavedJob.objects.filter(user=self.request.user)
-        saved_jobs_user = list(user_saved_jobs.values_list("job", flat=True))
-        jobs = map(lambda x: x.job, user_saved_jobs)
-
-        return render(
-            request=request,
-            template_name="signin/success.html",
-            context={
-                "user": request.user,
-                "jobs": jobs,
-                "saved_jobs_user": saved_jobs_user,
-            },
-        )
 
 
 class UserProfileView(View):
