1024553869422e7974f3be5bf640dc7209949cb4
Gaurav Agrawal <gauravag2207@gmail.com>
2020-11-10 11:17:14 -0600

    New model

--- signin/models.py
+++ signin/models.py
@@ -5,22 +5,23 @@ from examresults.models import CivilServicesTitle
 # Create your models here.
 
 
-class UsersCurrentCivilServiceTitle(models.Model):
+class UsersCivilServiceTitle(models.Model):
     civil_service_title = models.ForeignKey(
         CivilServicesTitle, on_delete=models.CASCADE
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_interested = models.BooleanField("is_interested", default=False)
 
     class Meta:
         unique_together = ("civil_service_title", "user")
 
 
-class UsersInterestedCivilServiceTitle(models.Model):
-    civil_service_title = models.ForeignKey(
-        CivilServicesTitle, on_delete=models.CASCADE
-    )
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    exam_number = models.CharField(max_length=200)
+# class UsersInterestedCivilServiceTitle(models.Model):
+#     civil_service_title = models.ForeignKey(
+#         CivilServicesTitle, on_delete=models.CASCADE
+#     )
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     exam_number = models.CharField(max_length=200)
 
-    class Meta:
-        unique_together = ("exam_number", "user")
+#     class Meta:
+#         unique_together = ("exam_number", "user")
