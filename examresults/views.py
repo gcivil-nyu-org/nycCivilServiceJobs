79b3e1ad63aa32f430a40538d80689a1575542cb
Savitha <savithag1197@gmail.com>
2020-11-09 20:59:01 -0500

    Modified Search Exam for Server-Side Pagination

--- examresults/views.py
+++ examresults/views.py
@@ -1,36 +1,70 @@
-from django.views import generic
 from examresults.models import ExamResultsActive
 from django.db.models import Q
+from django.views.generic import TemplateView
+from django_datatables_view.base_datatable_view import BaseDatatableView
 
 
-class ExamsActiveView(generic.ListView):
+class ExamsActiveView(TemplateView):
+    template_name = "examresults/exams.html"
     model = ExamResultsActive
     context_object_name = "exams"
     queryset = ExamResultsActive.objects.order_by("exam_number")
-    template_name = "examresults/exams.html"  # Specify your own template name/location
+    # template_name = "examresults/exams.html"  # Specify your own template name/location
     title_code = []
     title_description = []
 
     def dispatch(self, request, *args, **kwargs):
-        self.title_code = [
-            x["list_title_code"]
-            for x in ExamResultsActive.objects.values("list_title_code").distinct()
-        ]
+        query = self.request.GET.get("q")
+        queryset = ExamResultsActive.objects.all()
+
+        if query:
+            print("Dispatch", query)
+            if query.isnumeric():
+                queryset = ExamResultsActive.objects.filter(
+                    Q(exam_number=query) | Q(list_title_code=query)
+                )
+            else:
+
+                queryset = ExamResultsActive.objects.filter(
+                    list_title_desc__icontains=query
+                )
+
         self.title_desc = [
-            x["list_title_desc"]
-            for x in ExamResultsActive.objects.values("list_title_desc").distinct()
+            x["list_title_desc"] for x in queryset.values("list_title_desc").distinct()
         ]
         return super(ExamsActiveView, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["title_code"] = self.title_code
         context["title_desc"] = self.title_desc
-        print(len(self.title_code))
         return context
 
-    def get_queryset(self):
-        query = self.request.GET.get("q", None)
+
+class ExamJSON(BaseDatatableView):
+    model = ExamResultsActive
+    columns = [
+        "exam_number",
+        "list_title_code",
+        "list_title_desc",
+        "first_name",
+        "middle_initial",
+        "last_name",
+        "adjust_final_average",
+    ]
+    order_columns = [
+        "exam_number",
+        "list_title_code",
+        "list_title_desc",
+        "first_name",
+        "middle_initial",
+        "last_name",
+        "adjust_final_average",
+    ]
+
+    def get_initial_queryset(self, q=None):
+        query = self.request.GET.get("q")
+        print(self.request.GET)
+        exams = ExamResultsActive.objects.none()
         if query:
             if query.isnumeric():
                 exams = ExamResultsActive.objects.filter(
@@ -40,6 +74,4 @@ class ExamsActiveView(generic.ListView):
                 exams = ExamResultsActive.objects.filter(
                     list_title_desc__icontains=query
                 )
-
-            return exams
-        return None
+        return exams
